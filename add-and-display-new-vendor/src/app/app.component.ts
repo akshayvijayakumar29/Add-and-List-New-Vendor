792fa42b5c728e4882e6ee8acdb8d9c6468d5a08
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2025-01-22 17:54:32 +0530

    feat:new add vendor page

--- add-and-display-new-vendor/src/app/app.component.ts
+++ add-and-display-new-vendor/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./layout/ui/navbar/navbar.component";
+import { CreateVendorComponent } from "./layout/pages/layout/create-vendor/create-vendor.component";
+import { FooterComponent } from "./layout/ui/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CreateVendorComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

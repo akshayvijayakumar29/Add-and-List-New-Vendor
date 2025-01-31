681c438a40972b10712f7241eda49d85641cb10d
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2025-01-31 15:20:28 +0530

    enchancement:changed styles of radio button for service typr

--- add-and-display-new-vendor/src/app/layout/ui/contract-type-button/contract-type-button.component.ts
+++ add-and-display-new-vendor/src/app/layout/ui/contract-type-button/contract-type-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contract-type-button',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './contract-type-button.component.scss'
 })
 export class ContractTypeButtonComponent {
+
+
+  @Input() formControl: FormControl = new FormControl(); // Default initialization in constructor
+  @Output() valueChanged = new EventEmitter<any>(); // Emit value changes
+
+  onChange(value: string) {
+    this.formControl.setValue(value);  // Update the formControl value
+    this.valueChanged.emit(value); // Emit the new value
+  }
 
 }
